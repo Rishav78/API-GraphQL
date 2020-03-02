34142da8378c27f27e2d81b63e0d2f7445f20769
Rishav <rishavgarg789@gmail.com>
2020-03-02 11:51:40 +0530

    basic schema defined

--- app.js
+++ app.js
@@ -3,10 +3,34 @@ if ( process.env.NODE_ENV !== 'production' ) {
 }
 
 const express = require('express');
+const graphqlHttp = require('express-graphql');
+const { buildSchema  } = require('graphql');
 const PORT = process.env.PORT;
 const app = express();
 
+const schema = buildSchema(`
+    type RootQuery {
+        events: [String!]!
+    }
 
+    type RootMutation {
+        createEvent(name: String): String
+    }
+
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+`)
+
+app.use('/graphql', graphqlHttp({
+    schema,
+    rootValue: {
+        events: () => ['1', '2'],
+        createEvent: (args) => args.name
+    },
+    graphiql: true
+}));
 
 app.use(express.json());
 
